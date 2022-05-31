ce0b8de09cfe1c25640aaf356af0609fadc2abd7
Moana126 <hackercompilter@gmail.com>
2022-05-31 15:07:03 -0300

    surya: Elytra-Ify Signed-off-by: Moana <Hackercompilter@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_surya.mk
+    $(LOCAL_DIR)/elytra_surya.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_surya-user \
-    aosp_surya-userdebug \
-    aosp_surya-eng
+    elytra_surya-user \
+    elytra_surya-userdebug \
+    elytra_surya-eng
