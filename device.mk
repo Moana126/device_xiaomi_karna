e994f8396485b8f24ffdf7595ef9c0fc90bbbed5
Cow6oy <mezackisilva@gmail.com>
2022-06-01 16:20:27 -0300

    surya: Drop xiaomiparts for now

--- device.mk
+++ device.mk
@@ -106,15 +106,6 @@ PRODUCT_PACKAGES += \
     android.hardware.contexthub@1.0-impl.generic \
     android.hardware.contexthub@1.0-service
 
-# Device Settings
-PRODUCT_PACKAGES += \
-    XiaomiParts
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/parts/init.xiaomiparts.rc:$(TARGET_COPY_OUT_PRODUCT)/etc/init/init.xiaomiparts.rc \
-    $(LOCAL_PATH)/parts/init.xiaomiparts.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.xiaomiparts.rc \
-    $(LOCAL_PATH)/parts/privapp-permissions-parts.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-parts.xml
-
 PRODUCT_PRODUCT_PROPERTIES += \
     persist.lcd.hbm_mode=0 \
     persist.lcd.cabc_mode=1
